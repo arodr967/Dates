02e9bb0bdb1fb49079c8ad8537126a05bbc3e82c
Alicia Rodriguez <arodr967@fiu.edu>
2015-11-05 19:27:53 -0500

    Create FormatDates.c

---
+++ FormatDates.c
@@ -0,0 +1,27 @@
+/*
+ File Name: FormatDates.c
+ 
+ Description:  
+    Accept the validated dates in month/day/year format and convert
+    each of them to the day, abbreviated month & year format 
+    (i.e. 1 JAN 1990). The abbreviated month consist of the first
+    three letters of the month, capitalized.
+ 
+    The converted results will be redirected to the output file 
+    (output.dat), followed by a copy of the original (dates.dat) data.
+ 
+ Disclaimer:
+    I hereby certify that this collective work is my own
+    and none of it is the work of any other person or entity.
+ 
+ Created by Alicia Rodriguez on 11/5/15.
+ 
+ */
+
+#include "FormatDates.h"
+
+
+int main(int argc, char *argv[])
+{
+    
+}
