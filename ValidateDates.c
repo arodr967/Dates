c1fa3f7cf6b178eca19d2442de6147f7b93ede52
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-11-05 19:30:41 -0500

    Initial commit

---
+++ ValidateDates.c
@@ -0,0 +1,27 @@
+/*
+ File Name: ValidateDates.c
+  
+ Description:   
+    Accept input via command-line arguments. The input will be the number
+    of valid entries to be redirected from the dates input file
+    (dates.dat). A zero indicates to input all entries from the dates input
+    file. This input will then be piped out to the second program,
+    FormatDates.c.
+    
+    This program will also handle corrupt dates in the dates.dat file.
+ 
+ Disclaimer: 
+    I hereby certify that this collective work is my own
+    and none of it is the work of any other person or entity.
+ 
+ Created by Alicia Rodriguez on 11/5/15.
+
+*/
+
+#include "ValidateDates.h"
+
+
+int main(int argc, char *argv[])
+{
+    
+}
---
+++ ValidateDates.h
@@ -0,0 +1,20 @@
+/*
+ File Name: ValidateDates.h
+  
+ Description: 
+    Header file for the ValidateDates.c program.
+ 
+ Created by Alicia Rodriguez on 11/5/15.
+ 
+ */
+
+
+#ifndef ValidateDates_h
+#define ValidateDates_h
+
+#include <stdio.h>
+
+
+
+
+#endif /* ValidateDates_h */
