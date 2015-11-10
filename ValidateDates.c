66686b2c4f9ee2f372ea77f87055a567347871b6
Alicia Rodriguez <aliciarodriguez@alicias-macbook-pro.local>
2015-11-10 14:54:08 -0500

    Added comments and fixed issue when trying to read the first line of the file.

--- ValidateDates.c
+++ ValidateDates.c
@@ -15,6 +15,7 @@
     and none of it is the work of any other person or entity.
  
  Created by Alicia Rodriguez.
+ Panther ID: 5162522
 
 */
 
@@ -27,26 +28,36 @@
 #define ARG_NUM 2 /* Max number of arguments. The first one, argv[0], is the
                      program and the second one, argv[1] is the number which 
                      we want to read up to. */
-#define MAXLINE 1000
-#define INPUT 1
+#define MAXLINE 100 /* The maximum number of entries each line can contain */
+#define INPUT 1 /* The input is in argument 1 (e.g. argv[1]) */
 
 /* Start at 1 until 12 for each Month. */
 enum months {JAN = 1, FEB, MAR, APR, MAY, JUN, JUL, AUG, SEP, OCT, NOV, DEC};
 
 /* Prototypes */
-int getLine(char s[], int limit);
+int getLine(char line[], int limit);
 int validate(int month, int day, int year);
 void getDate(int month, int day, int year);
 
+
+/* main function: accept input via command-line arguments and 
+   generate the dates. */
 int main(int argc, char *argv[])
 {
-    int day, month, year;
+    int day, month, year, input; /* Input, for the number of lines we want
+                                    to read from the file */
     char line[MAXLINE + 1];
-    int input;
     
     if(argc == ARG_NUM)
     {
         input = atoi(argv[INPUT]);
+        
+        if(input > INT_MAX) {
+            input = INT_MAX; /* To make sure that the input number does not
+                                exceed the int maximum. */
+        }
+        
+        getDate(month, day, year); /* Read the first line of the file */
         
         if(input <= 0) /* Input ALL valid dates */
         {
@@ -56,7 +67,7 @@ int main(int argc, char *argv[])
             }
         } else { /* Input up to the input provided of valid dates */
             
-            while(input >= 0 && getLine(line, sizeof(line)) > 0)
+            while(input > 0 && getLine(line, sizeof(line)) > 0)
             {
                 getDate(month, day, year);
                 input--; /* Decrement the input */
@@ -69,24 +80,23 @@ int main(int argc, char *argv[])
 /* validate function: validate the date. */
 int validate(int month, int day, int year)
 {
-    int leapYear = ((year%4 == 0 && year % 100 != 0) || year % 400 == 0); /* test for leap year */
+    /* test for leap years: */
+    int leapYear = ((year%4 == 0 && year % 100 != 0) || year % 400 == 0);
+    
     
     if((month >= 1 && month <= 12) && (day >= 1 && day <= 31))
     {
         if((month == JAN || month == MAR || month == MAY || month == JUL || month == AUG || month == OCT || month == DEC))
         {
             return TRUE;
-        }
-        if((day >= 1 && day <= 30) && (month == APR || month == JUN || month == SEP || month == NOV))
+        } else if((day >= 1 && day <= 30) && (month == APR || month == JUN || month == SEP || month == NOV))
+        {
+            return TRUE;
+        } else if(((day >1 && day <= 28) && month == FEB && !leapYear) || ((day >1 && day <= 29) && month == FEB && leapYear))
         {
             return TRUE;
         }
-        
-        if( ((day >1 && day <= 28) && month == FEB && !leapYear) || ((day >1 && day <= 29) && month == FEB && leapYear))
-        {
-            return TRUE;
-        }
-    }
+    } 
     
     return FALSE;
 }
@@ -96,31 +106,31 @@ void getDate(int month, int day, int year)
 {
     if(scanf("%d/%d/%d", &month, &day, &year) == 3)
     {
-        if(validate(month, day, year) == TRUE)
+        if(validate(month, day, year))
         {
+            /* Print here to the second program in the day, month, year format */
             printf("%d/%d/%d\n", day, month, year);
         }
     }
 }
 
-
-/* getLine function: a line int a string and return the length of the string. */
-int getLine(char s[], int limit)
+/* getLine function: get a line and return the length of the string. */
+int getLine(char line[], int limit)
 {
     int c, i;
     
-    for (i = 0 ; i < limit-1 && (c=getchar()) != EOF && c !='\n' ; ++i)
+    for (i = 0 ; i < limit-1 && ((c=getchar()) != EOF) && c !='\n' ; ++i)
     {
-        s[i] = c;
+        line[i] = c;
     }
-        
+    
     if (c == '\n')
     {
-        s[i] = c;
+        line[i] = c;
         ++i;
     }
     
     /* Add the end of file character (null) after it is done reading */
-    s[i] = '\0';
+    line[i] = '\0';
     return i;
 }
